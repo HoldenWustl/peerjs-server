1c6cf2733ace6ef077ba9152e1f13be5589bd979
HoldenWustl <h.j.weinberg@wustl.edu>
2025-02-02 01:23:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { PeerServer } = require('peer');
 const peerServer = PeerServer({
   port: process.env.PORT || 443, // Railway uses dynamic ports, so use process.env.PORT
   path: '/myapp',
+  secure: true,
   proxied: true,  // Needed for hosting behind Railway
   debug: true
 });
