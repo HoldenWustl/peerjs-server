1e7a62f4188dedac27fd511dc079c27e51f08051
HoldenWustl <h.j.weinberg@wustl.edu>
2025-02-02 00:44:34 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({
+  port: process.env.PORT || 443, // Railway uses dynamic ports, so use process.env.PORT
+  path: '/myapp',
+  proxied: true,  // Needed for hosting behind Railway
+  debug: true
+});
+
+console.log("PeerJS signaling server is running...");
